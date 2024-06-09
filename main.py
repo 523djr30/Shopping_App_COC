4f87c905e186c48be2a11615c4ba74a05b79c4dd
523djr30 <56850322+523djr30@users.noreply.github.com>
2024-06-09 01:27:25 -0500

    Profile Interface Finished, Cart Interface WIP

--- main.py
+++ main.py
@@ -172,6 +172,9 @@ class ShopperProfile:
     
     def update_balance(self, new_balance):
         self.balance = new_balance
+        
+    def update_name(self, new_name):
+        self.name = new_name
 
     def build_from_str(line):
         split_line = line.split(',')
@@ -203,6 +206,7 @@ class ConsoleInterface:
         "s": "(S)earch",
         "r": "(R)eset filters",
         "c": "(C)art",
+        "p": "(P)rofile",
         "q": "(Q)uit !! LOSE ALL PROGRESS !!"
     }
     cart_menu = {
@@ -231,13 +235,11 @@ class ConsoleInterface:
     # --- Cart --- #
     def cart_interface(self):
         self.show_cart_options()
-        usr_input = input("Select an option: ")
-        usr_input = usr_input.lower()
-        while (usr_input != "b"):
-            self.cart_handler(usr_input)
+        user_input = input("Select an option: ").lower()
+        while (user_input != "b"):
+            self.cart_handler(user_input)
             self.show_cart_options()
-            usr_input = input("Select an option: ")
-            usr_input = usr_input.lower()
+            user_input = input("Select an option: ").lower()
     def show_cart_options(self):
         print(f"{'':-^30}")
         print(f"{' Cart Options ':-^30}\n")
@@ -305,7 +307,7 @@ class ConsoleInterface:
         print(f"Item removed: {item_removed.name}\n")
     def purchase(self):
         purchase_ui = PurchaseInterface(self.profile,self.cart)
-        purchase_ui.purchase_ui()
+        purchase_ui.ui()
 
     # --- Search --- #
     def search(self):
@@ -388,6 +390,11 @@ class ConsoleInterface:
         print(f"{'NEW LIST':-^30}")
         self.display_items()
 
+    # --- Profile --- #
+    def profile_interface(self):
+        profile_interface = ProfileInterface(self.profile)
+        profile_interface.ui()
+
     # --- Menu --- #
     def menu(self):
         self.show_main_options()
@@ -416,6 +423,8 @@ class ConsoleInterface:
                 self.reset_filters()
             case "c":
                 self.cart_interface()
+            case "p":
+                self.profile_interface()
             case _:
                 print("Invalid input. Try again.")
     
@@ -439,28 +448,180 @@ class ConsoleInterface:
     def reset_filters(self):
         self.curr_list = self.db.items
 
+class CartInterface:
+    options = {
+        "a": "(A)dd item to cart",
+        "v": "(V)iew cart",
+        "r": "(R)emove item from cart",
+        "c": "(C)lear cart",
+        "p": "(P)urchase items",
+        "b": "(B)ack"
+    }
+    def __init__(self, cart, profile):
+        self.cart = cart
+        self.profile = profile
+    def ui(self):
+        self.show_options()
+        user_input = input("Select an option: ").lower()
+        while (user_input != "b"):
+            self.input_handler(user_input)
+            self.show_options()
+            user_input = input("Select an option: ").lower()
+    def show_options(self):
+        print(f"{'':-^30}")
+        print(f"{' Cart Options ':-^30}\n")
+        for key in self.cart_menu.keys():
+            print(f"{key}: {self.cart_menu[key]}\n")
+        print(f"{'':-^30}\n")
+        print("\n")
+    def input_handler(self,usr_input):
+        usr_input = usr_input.lower()
+        match usr_input:
+            case "a":
+                self.add_to_cart()
+            case "v":
+                self.show_cart()
+            case "r":
+                self.remove_from_cart()
+            case "c":
+                self.clear_cart()
+            case "p":
+                self.purchase()
+            case _:
+                print("Invalid input. Try again.\n")
+    def clear_cart(self):
+        self.cart.clear_cart()
+    def add_to_cart(self):
+        self.display_items_indexed()
+        length = len(self.curr_list)
+        usr_input = input("Which item do you want? Type its number: ")
+        try:
+            usr_input = int(usr_input)
+        except:
+            print("Invalid input. Must be a non-negative number. Try again.\n")
+            return
+        if(usr_input > length):
+            print("Number is too high. Try Again.\n")
+            return
+        elif(usr_input < 1):
+            print("Number is too low. Try again.\n")
+            return
+        item_added = self.curr_list[usr_input - 1]
+        self.cart.add_to_cart(item_added)
+        formatted_item_price = f"{float(item_added.price):.2f}"
+        print(f"Item added: {item_added.name: <30} {formatted_item_price: >8}\n")
+    def show_cart(self):
+        print(self.cart.show_cart_str())
+    def remove_from_cart(self):
+        self.show_cart()
+        cart_items = self.cart.get_items()
+        length = len(cart_items)
+        usr_input = input("Which item do you want to remove? Type its number: ")
+        try:
+            usr_input = int(usr_input)
+        except:
+            print("Invalid input. Must be a non-negative number. Try again.\n")
+            return
+        if(usr_input > length):
+            print("Number is too high. Try Again.\n")
+            return
+        elif(usr_input < 1):
+            print("Number is too low. Try again.\n")
+            return
+        index = int(usr_input) - 1
+        item_removed = cart_items[index]
+        self.cart.remove_item(index)
+        print(f"Item removed: {item_removed.name}\n")
+    def purchase(self):
+        purchase_ui = PurchaseInterface(self.profile,self.cart)
+        purchase_ui.ui()
+
+class ProfileInterface:
+    options = {
+        "v": "(V)iew profile",
+        "n": "Change (n)ame",
+        "m": "Change balance in (m)arks",
+        "b": "(B)ack"
+    }
+
+    def __init__(self, profile):
+        self.profile = profile
+
+    def ui(self):
+        self.show_options()
+        user_input = input("Select an option: ").lower()
+        while (user_input != "b"):
+            self.input_handler(user_input)
+            self.show_options()
+            user_input = input("Select an option: ").lower()
+
+    def input_handler(self, user_input):
+        match user_input:
+            case "v":
+                self.show_profile()
+            case "n":
+                self.name_change()
+            case "m":
+                self.balance_change()
+            case _:
+                print("Invalid input. Try again.")
+
+    def show_options(self):
+        print(f"{'':-^30}")
+        print(f"{' Profile Options ':-^30}\n")
+        for key in self.options.keys():
+            print(f"{key}: {self.options[key]}\n")
+        print(f"{'':-^30}\n")
+        print("\n")
+    
+    def show_profile(self):
+        print(f"\n{' Profile ':-^30}")
+        print(f"Name: {self.profile.name: >15}")
+        formatted_bal = f"{float(self.profile.balance):.2f}"
+        print(f"Balance: {formatted_bal: >15}")
+        print(f"{'':-<30}")
+    
+    def name_change(self):
+        self.show_profile()
+        print("Above is your current profile.")
+        new_name = input("What name would you like to have?: ")
+        self.profile.update_name(new_name)
+        self.show_profile()
+        print("Your edited profile is shown above.")
+    
+    def balance_change(self):
+        self.show_profile()
+        print("Above is your current profile.")
+        new_balance = input("Enter new balance: ")
+        try:
+            new_balance = float(new_balance)
+        except:
+            print("Not a valid input. It must be a number without spaces or commas. Try again.")
+            return
+        self.profile.update_balance(new_balance)
+        self.show_profile()
+        print("Your edited profile is shown above.") 
+
 class PurchaseInterface:
     def __init__(self, profile, cart):
         self.cart = cart
         self.profile = profile
     
-    purchase_options = {
+    options = {
         "v": "(V)iew bill",
         "c": "(C)omplete purchase",
         "b": "(B)ack"    
     }
     
-    def purchase_ui(self):
-        self.show_purchase_options()
-        usr_input = input("Select an option: ")
-        usr_input = usr_input.lower()
-        while (usr_input != "b"):
-            self.handler(usr_input)
-            self.show_purchase_options()
-            usr_input = input("Select an option: ")
-            usr_input = usr_input.lower()
+    def ui(self):
+        self.show_options()
+        user_input = input("Select an option: ").lower()
+        while (user_input != "b"):
+            self.input_handler(user_input)
+            self.show_options()
+            user_input = input("Select an option: ").lower()
     
-    def handler(self,usr_input):
+    def input_handler(self,usr_input):
         usr_input = usr_input.lower()
         match usr_input:
             case "v":
@@ -470,11 +631,11 @@ class PurchaseInterface:
             case _:
                 print("Invalid input. Try again.")
 
-    def show_purchase_options(self):
+    def show_options(self):
         print(f"{'':-^30}")
-        print(f"{' Search Options ':-^30}\n")
-        for key in self.purchase_options.keys():
-            print(f"{key}: {self.purchase_options[key]}\n")
+        print(f"{' Purchase Options ':-^30}\n")
+        for key in self.options.keys():
+            print(f"{key}: {self.options[key]}\n")
         print(f"{'':-^30}\n")
         print("\n")
 
@@ -507,6 +668,7 @@ def main():
         curr_usr = ShopperProfile.build_from_txt('./saves/profile.txt')
     else:
         curr_usr = ShopperProfile.build_from_input()
+        ShopperProfile.save_to_txt(curr_usr)
 
     # --- Create Dataframe ---
     db = ItemsDB('./saves/items_COC.csv')
