3913dff69167a3457464c13faea396519d42cbde
523djr30 <56850322+523djr30@users.noreply.github.com>
2024-06-09 16:15:07 -0500

    Maybe new distro 1.1

---
+++ main.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=C:\Users\DANIEL~1.ROB\AppData\Local\Programs\Python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\Daniel J. Robertson\AppData\Local\Programs\Python\Python310
+set NUITKA_PYTHONPATH=C:\Users\Daniel J. Robertson\Documents\COC TTRPG\Shopping_App;C:\Users\Daniel J. Robertson\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Daniel J. Robertson\AppData\Local\Programs\Python\Python310\lib;C:\Users\Daniel J. Robertson\AppData\Local\Programs\Python\Python310;C:\Users\Daniel J. Robertson\AppData\Local\Programs\Python\Python310\lib\site-packages;C:\Users\Daniel J. Robertson\AppData\Local\Programs\Python\Python310\lib\site-packages\win32;C:\Users\Daniel J. Robertson\AppData\Local\Programs\Python\Python310\lib\site-packages\win32\lib;C:\Users\Daniel J. Robertson\AppData\Local\Programs\Python\Python310\lib\site-packages\Pythonwin
+"%~dp0main.exe" %*
